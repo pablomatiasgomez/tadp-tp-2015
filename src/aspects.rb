6d8082a763a3d2a24f2a94c4d6dea5ce7361fe46
j9794 <juanmanuel9794@hotmail.com>
2015-09-11 08:58:03 -0300

    Refactor funcionaloso.

--- src/aspects.rb
+++ src/aspects.rb
@@ -3,15 +3,16 @@ require_relative 'origin.rb'
 class Aspects
 
   def self.regex_to_origins(regex)
-    Object.constants.grep(regex).map {|regex_symbol| Object.const_get(regex_symbol)}
+    Object.constants.grep(regex)
+                    .map {|regex_symbol| Object.const_get(regex_symbol)}
+                    .select{|constant|constant.is_a?(Class ) || constant.is_a?(Module)}
   end
 
   def self.find_origins(*possible_origins)
-    origins = []
 
-    possible_origins.map do |possible_origin|
-      possible_origin.is_a?(Regexp) ? origins+= regex_to_origins(possible_origin) : origins+= [possible_origin]
-    end
+    origins = possible_origins.flat_map {
+        |possible_origin| possible_origin.is_a?(Regexp) ? regex_to_origins(possible_origin) : [possible_origin]
+    }
 
     origins.empty? ? (raise 'Error: Empty Origin') : origins.uniq
   end
