49d7b6834b10e07cd04f4ed47eb50daf31deaf08
gcrespi <crespi.gustavo@gmail.com>
2015-09-11 21:13:56 -0300

    Refactor Superador

--- src/aspects.rb
+++ src/aspects.rb
@@ -10,7 +10,7 @@ class Aspects
     origins = possible_origins.flat_map { |possible_origin|
                                 possible_origin.is_a?(Regexp) ? regex_to_origins(possible_origin) : [possible_origin] }
 
-    origins.empty? ? (raise EmptyOriginException.new("Cant call Aspects.on without oirigins to transform"))
+    origins.empty? ? (raise EmptyOriginException.new("Can't call Aspects.on without origins to transform"))
                    : origins.uniq
   end
 
--- src/origin.rb
+++ src/origin.rb
@@ -2,21 +2,22 @@ require_relative 'conditions.rb'
 require_relative 'transforms.rb'
 
 class Origin
-  attr_accessor :origin
+  include WithCondition
+  attr_accessor :target_origin
 
   def initialize(origin)
-    @origin=origin
+    @target_origin=aspects_target(origin)
   end
 
   def where(*conditions)
-    origin_methods(origin).select { |origin_method| conditions.all? {|condition| condition.call(origin,origin_method)} }
+    origin_methods(target_origin).select { |origin_method| conditions.all? {|condition| condition.call(target_origin,origin_method)} }
   end
 
   def transform(origin_methods, &block)
     origin_methods.each do |method|
-      optimus_prime = Transformer.new(origin_method(origin, method))
+      optimus_prime = Transformer.new(target_origin.instance_method(method))
       optimus_prime.instance_eval &block
-      origin_define_method(origin,method,&(optimus_prime.transform_method))
+      target_origin.send(:define_method, method, &(optimus_prime.transform_method))
     end
   end
 
@@ -24,34 +25,7 @@ class Origin
     origin.is_a?(Module) ? origin : origin.singleton_class
   end
 
-  def origin_methods(origin)
-    (aspects_target(origin).instance_methods)+(aspects_target(origin).private_instance_methods)
+  def origin_methods(target_origin)
+    (target_origin.instance_methods)+(target_origin.private_instance_methods)
   end
-
-  def origin_method(origin, method_sym)
-    aspects_target(origin).instance_method(method_sym)
-  end
-
-  def origin_public_methods(origin)
-    aspects_target(origin).public_instance_methods
-  end
-
-  def origin_private_methods(origin)
-    aspects_target(origin).private_instance_methods
-  end
-
-  def origin_define_method(origin,method_name,&logic)
-    aspects_target(origin).send(:define_method, method_name, &logic)
-  end
-
-# Alternativa Aserrin con dulce de leche (Casi Superadora)
-# { :origin_method => :instance_method,
-#   :origin_public_methods => :public_instance_methods,
-#   :origin_private_methods => :private_instance_methods,
-#   :origin_define_method => :define_method}.each_pair do |my_method, original_method|
-#   define_method(my_method) do |origin,*parameters,&block|
-#     aspects_target(origin).send(original_method,*parameters,&block)
-#   end
-# end
-
 end
