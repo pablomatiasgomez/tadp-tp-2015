2a1248fe35aea56398ce089eab7a4cf3abf49bb6
GastonTS <gastonsantoalla@gmail.com>
2015-09-14 16:55:10 -0300

    Precedence to lists

--- src/transforms.rb
+++ src/transforms.rb
@@ -7,72 +7,64 @@ class Transformer
     @origin = origin
     @original_method = @method = origin.instance_method(method_to_transform)
     @transformations = []
+    @transformations.define_singleton_method(:[]){|n| super(n) || self[n]=[]}
   end
 
   def transform_method(&transforms)
     instance_eval &transforms
 
-    transformations=@transformations
-    method=@method
+    transformations = @transformations
+    method = @method
 
     @origin.send(:define_method, @original_method.name) do
       |*args, &arg_block|
       method = method.bind(self) if method.is_a?(UnboundMethod)
-      instance_exec_b(arg_block, *args, &transformations.reduce(method) {
+      instance_exec_b(arg_block, *args, &transformations.compact.flatten.reduce(method) {
                                    |method, transformation, &arg_block|instance_exec_b(arg_block, method, &transformation) })
       end
   end
 
-  def add_transformation(&transformation)
-    @transformations << proc { |next_method| proc{|*args, &arg_block| instance_exec_b(arg_block, next_method, *args, &transformation) } }
-  end
-
-  def replace_method(&new_method)
-    @method = new_method
+  def add_transformation(precedence, &transformation)
+    @transformations[precedence] << proc { |next_method| proc{ |*args, &arg_block| instance_exec_b(arg_block, next_method, *args, &transformation) } }
   end
 
   def inject(hash)
-    method_parameter_names=@original_method.parameters.map { |_, n| n }
-    method_name=@original_method.name
+    method_parameter_names = @original_method.parameters.map { |_, n| n }
+    method_name = @original_method.name
     hash.keys.each { |key|
       unless method_parameter_names.include?key
         raise NoParameterException.new("Cant inject #{key}, the method doesn't have that parameter")
-      end
-      }
+      end }
 
-    add_transformation { |next_method, *args, &arg_block|
+    add_transformation(2) { |next_method, *args, &arg_block|
     method_parameter_names.each_with_index { |arg_name, index|
       if hash.key?(arg_name)
         args[index] = (hash[arg_name].is_a?Proc) ? hash[arg_name].call(self, method_name, args[index]) : hash[arg_name]
-      end
-    }
+      end }
 
-    instance_exec_b(arg_block, *args, &next_method)
-    }
+    instance_exec_b(arg_block, *args, &next_method) }
   end
 
   def before(&logic)
-    add_transformation{|next_method, *args, &arg_block|
-      cont = proc { |_, _, *new_parameters, &arg_block| instance_exec_b(arg_block, *new_parameters, &next_method)}
-      instance_exec_b(arg_block, self, cont, *args, &logic)
-    }
+    add_transformation(1) {|next_method, *args, &arg_block|
+      cont = proc { |_, _, *new_parameters, &arg_block| instance_exec_b(arg_block, *new_parameters, &next_method) }
+      instance_exec_b(arg_block, self, cont, *args, &logic) }
   end
 
 
   def after(&logic)
-    add_transformation{|next_method, *args, &arg_block|
+    add_transformation(1) { |next_method, *args, &arg_block|
       instance_exec_b(arg_block, *args, &next_method)
-      instance_exec_b(arg_block, self, *args, &logic)
-    }
+      instance_exec_b(arg_block, self, *args, &logic) }
   end
 
   def instead_of(&logic)
-    replace_method { |*args, &arg_block| instance_exec_b(arg_block, self, *args, &logic) }
+    add_transformation(0) { |_, *args, &arg_block| instance_exec_b(arg_block, self, *args, &logic) }
   end
 
   def redirect_to(target)
-    method_name=@original_method.name
-    replace_method { |*args, &arg_block|target.send(method_name, *args, &arg_block) }
+    method_name = @original_method.name
+    add_transformation(0) { |_, *args, &arg_block|target.send(method_name, *args, &arg_block) }
   end
 
 end
