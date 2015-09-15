e49d9b83542076b9df26fc25b059f4097584bb95
J9794 <juanmanuel9794@hotmail.com>
2015-09-15 13:04:49 -0300

    Primer parametro de instance_exec_b ya no se evalua en el contexto de la instancia.

--- src/object_extension.rb
+++ src/object_extension.rb
@@ -1,17 +1,13 @@
 class Object
 
   def instance_exec_b(param_block, *args, &method_block)
-    self.class.send(:define_method, :__juegos_de_azar__, &method_block)
-    param_block ? self.class.send(:define_method, :__mujerzuelas__, &param_block)
-                : self.class.send(:define_method, :__mujerzuelas__, proc)
+    self.class.send(:define_method, :__juegos_de_azar_y_mujerzuelas__, &method_block)
 
-    posta_method = self.method(:__juegos_de_azar__)
-    param_method=self.method(:__mujerzuelas__)
+    posta_method = self.method(:__juegos_de_azar_y_mujerzuelas__)
 
-    self.class.send(:remove_method, :__juegos_de_azar__)
-    self.class.send(:remove_method, :__mujerzuelas__)
+    self.class.send(:remove_method, :__juegos_de_azar_y_mujerzuelas__)
 
-    posta_method.call(*args, &param_method)
+    posta_method.call(*args, &param_block)
   end
 
 end
