af194df5da661a35f346610a03281da38566780c
GastonTS <gastonsantoalla@gmail.com>
2015-09-26 01:32:55 -0300

    Haciendo devuelta loq ue juan borro por paja de mergear

--- src/transforms.rb
+++ src/transforms.rb
@@ -1,22 +1,28 @@
 require_relative 'object_extension.rb'
 
 class MultiLevelQueue < Array
+
   def [](n)
-    super(n) || self[n]=[]
+    super(n) || self[n] = []
   end
+
   def to_list
     compact.flatten
   end
+
 end
 
 class Transformation
-  def initialize(&block)
-    @transformation_block=block
+
+  def initialize(&transformation)
+    @transformation = transformation
   end
+
   def transform(method)
-    transformation=@transformation_block
+    transformation=@transformation
     proc { |*args, &arg_block| instance_exec_b(arg_block, method, *args, &transformation) }
   end
+
 end
 
 
@@ -33,9 +39,9 @@ class Transformer
     instance_eval &transforms
 
     transformations = self.transformations.to_list
-    original_method = self.original_method
+    original_method = @original_method
 
-    @origin.send(:define_method, @original_method.name) do  |*args, &arg_block|
+    @origin.send(:define_method, original_method.name) do  |*args, &arg_block|
       transformed_method = transformations.reduce(original_method.bind(self)){ |method, transformation| transformation.transform(method) }
       instance_exec_b(arg_block, *args, &transformed_method)
     end
@@ -45,42 +51,43 @@ class Transformer
     @transformations[precedence] << Transformation.new(&transformation_block)
   end
 
-  def inject(hash,precedence=2)
+  def inject(precedence = 2, hash)
     method_parameter_names = @original_method.parameters.map { |_, n| n }
     method_name = @original_method.name
+
     hash.keys.each { |key|
       raise NoParameterException.new("Cant inject #{key}, #{method_name} doesn't have that parameter") unless method_parameter_names.include?key
     }
 
     before(precedence) do |original_method, *args, &arg_block|
-    method_parameter_names.each_with_index do |arg_name, index|
-      if hash.key?(arg_name)
-        args[index] = (hash[arg_name].is_a?Proc) ? hash[arg_name].call(self, method_name, args[index]) : hash[arg_name]
+      method_parameter_names.each_with_index do |arg_name, index|
+        if hash.key?(arg_name)
+          args[index] = (hash[arg_name].is_a?Proc) ? hash[arg_name].call(self, method_name, args[index]) : hash[arg_name]
+        end
       end
-    end
 
-    instance_exec_b(arg_block, *args, &original_method)
+      instance_exec_b(arg_block, *args, &original_method)
     end
   end
 
-  def before(precedence=1,&before_logic)
+  def before(precedence = 1, &before_logic)
     add_transformation(precedence) {|original_method, *args, &arg_block|
       instance_exec_b(arg_block, original_method, *args, &before_logic) }
   end
 
 
-  def after(precedence=1,&after_logic)
-    before(precedence) do |original_method,*args,&arg_block|
+  def after(precedence = 1, &after_logic)
+    before(precedence) do |original_method, *args, &arg_block|
       instance_exec_b(arg_block, *args, &original_method)
       instance_exec_b(arg_block, *args,&after_logic)
     end
   end
 
-  def instead_of(precedence=0,&instead_of_logic)
-    before(precedence) do |_,*args,&arg_block| instance_exec_b(arg_block,*args,&instead_of_logic) end
+  def instead_of(precedence = 0, &instead_of_logic)
+    before(precedence) do |_, *args, &arg_block| instance_exec_b(arg_block, *args, &instead_of_logic) end
   end
 
-  def redirect_to(target,precedence=0)
+  def redirect_to(precedence = 0, target)
     method_name = @original_method.name
     before(precedence) do |_,*args,&arg_block| target.send(method_name,*args,&arg_block) end
   end
