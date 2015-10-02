a7ebebdc95e8b0d9b3aa7b28942040753adf1908
J9794 <juanmanuel9794@hotmail.com>
2015-10-02 19:06:31 -0300

    Instance_exec_b define el metodo en la singleton_class en vez de en class.

--- src/object_extension.rb
+++ src/object_extension.rb
@@ -1,11 +1,11 @@
 class Object
 
   def instance_exec_b param_block, *args, &method_block
-    self.class.send :define_method, :__juegos_de_azar_y_mujerzuelas__, &method_block
+    self.singleton_class.send :define_method, :__juegos_de_azar_y_mujerzuelas__, &method_block
 
     method = self.method :__juegos_de_azar_y_mujerzuelas__
 
-    self.class.send :remove_method, :__juegos_de_azar_y_mujerzuelas__
+    self.singleton_class.send :remove_method, :__juegos_de_azar_y_mujerzuelas__
 
     method.call *args, &param_block
   end
